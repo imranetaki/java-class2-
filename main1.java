25911e1c8196bc938f72e839afdfc5e0b98ee7bb
imranetaki <146876470+imranetaki@users.noreply.github.com>
2023-11-19 22:12:50 +0100

    Add files via upload

---
+++ main1.java
@@ -0,0 +1,13 @@
+public class main1 {
+    public static void main(String[] args) {
+        notation firstnotation = new notation() ;
+        firstnotation.firstname1 = "Oussama" ;
+        firstnotation.lastname1 = "Bachir";
+        firstnotation.note1 = 15.5f ;
+        firstnotation.note2 =  14.75f ;
+        firstnotation.note3 = 17 ;
+
+        firstnotation.moy();
+
+    }
+}
---
+++ notation.java
@@ -0,0 +1,15 @@
+public class notation {
+    String firstname1 ;
+    String lastname1 ;
+    float note1 ;
+    float note2 ;
+    float note3 ;
+
+
+    void moy () {
+        System.out.println(firstname1 + " " + lastname1 + " a la note general : " + (note1+note2+note3)/3 ) ;
+
+
+        }
+    }
+
